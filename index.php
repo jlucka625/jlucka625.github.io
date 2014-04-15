a7cc26737ffffcd80c72959451f621921af6fc01
jlucka625 <jozzle06@ufl.edu>
2014-04-15 14:47:29 -0400

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include './include/before.html'; ?>
-
-<div class="well">
-	<p>Blah blah blah</p>
-	<button type="button" class="btn btn-info">Download</button>
-</div>
-
-<?php include './include/after.html'; ?>
-
