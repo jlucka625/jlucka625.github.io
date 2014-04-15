265c21affe7bd0b4d7e06e723bfd628472d95a98
jlucka625 <jozzle06@ufl.edu>
2014-04-15 14:41:35 -0400

    Using php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include './include/before.html'; ?>
+
+<div class="well">
+	<p>Blah blah blah</p>
+	<button type="button" class="btn btn-info">Download</button>
+</div>
+
+<?php include './include/after.html'; ?>
+
